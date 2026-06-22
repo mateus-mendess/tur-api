fb4f9ab268f7ae16ff2082c0669f2abac4c6a8fa
mateus-mendess <mendes.profissional12@gmail.com>
2026-06-22 17:39:29 -0300

    feat: initial commit.

---
+++ src/main/java/com/m2/tur/TurApplication.java
@@ -0,0 +1,13 @@
+package com.m2.tur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2/tur/TurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2.tur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
