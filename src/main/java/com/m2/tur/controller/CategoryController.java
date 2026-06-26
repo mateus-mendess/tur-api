fe6f0c1f30314b833d970b00af13adb960920691
mateus-mendess <mendes.profissional12@gmail.com>
2026-06-25 22:19:22 -0300

    feat: add category list for tourist attraction registration and filters.

--- src/main/java/com/m2/tur/controller/CategoryController.java
+++ src/main/java/com/m2/tur/controller/CategoryController.java
@@ -1,21 +1,26 @@
 package com.m2.tur.controller;
 
 import com.m2.tur.model.dto.request.CategoryRequest;
+import com.m2.tur.model.dto.response.CategoryResponse;
 import com.m2.tur.service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
     private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getCategories() {
+        return ResponseEntity.ok(categoryService.findAllCategories());
+    }
 
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid CategoryRequest request) {
--- src/main/java/com/m2/tur/service/CategoryService.java
+++ src/main/java/com/m2/tur/service/CategoryService.java
@@ -4,11 +4,15 @@ import com.m2.tur.infra.exception.CategoryAlreadyExistsException;
 import com.m2.tur.infra.exception.NotFoundException;
 import com.m2.tur.mapper.CategoryMapper;
 import com.m2.tur.model.dto.request.CategoryRequest;
+import com.m2.tur.model.dto.response.CategoryResponse;
 import com.m2.tur.model.entity.Category;
 import com.m2.tur.model.entity.User;
 import com.m2.tur.model.repository.CategoryRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +21,14 @@ public class CategoryService {
     private final CategoryMapper categoryMapper;
     private final AuthService authService;
 
+    public List<CategoryResponse> findAllCategories() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::toResponse)
+                .toList();
+    }
+
+    @Transactional
     public void save(CategoryRequest request) {
         validate(request);
 
