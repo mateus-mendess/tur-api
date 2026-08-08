f7aa98d443967ec83cd833affb979af969411984
mateus-mendess <mendes.profissional12@gmail.com>
2026-08-07 22:15:11 -0300

    fix(UserController): add the @Valid annotation to the create method

--- src/main/java/com/m2/tur/controller/UserController.java
+++ src/main/java/com/m2/tur/controller/UserController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,7 +31,7 @@ public class UserController {
             @ApiResponse(responseCode = "400", description = "Invalid request data or email already in use.")
     })
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody UserRequest request) {
+    public ResponseEntity<?> create(@RequestBody @Valid UserRequest request) {
         userService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
